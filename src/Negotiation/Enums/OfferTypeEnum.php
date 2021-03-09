67b33377fc86dad1f208b1f624d9f2d9abef9a97
Sergey <sergo920@yahoo.com>
2021-03-09 16:49:57 +0200

    Add files via upload

---
+++ src/Negotiation/Enums/OfferTypeEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DTS\eBaySDK\Negotiation\Enums;
+
+/**
+ * Created by PhpStorm.
+ * User: sergo
+ * Date: 6/10/20
+ * Time: 11:18 PM
+ */
+class OfferTypeEnum
+{
+    const SELLER_INITIATED_OFFER = 'SELLER_INITIATED_OFFER';
+}
---
+++ src/Negotiation/Enums/TimeDurationUnitEnum.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergo
+ * Date: 6/10/20
+ * Time: 11:24 PM
+ */
+
+namespace DTS\eBaySDK\Negotiation\Enums;
+
+class TimeDurationUnitEnum
+{
+    const C_BUSINESS_DAY = 'BUSINESS_DAY';
+    const C_CALENDAR_DAY = 'CALENDAR_DAY';
+    const C_DAY = 'DAY';
+    const C_HOUR = 'HOUR';
+    const C_MILLISECOND = 'MILLISECOND';
+    const C_MINUTE = 'MINUTE';
+    const C_MONTH = 'MONTH';
+    const C_SECOND = 'SECOND';
+    const C_YEAR = 'YEAR';
+}
