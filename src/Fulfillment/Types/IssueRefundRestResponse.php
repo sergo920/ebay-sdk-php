4535ed85e3e2e6556a19cde9405ad75392ecd88a
Sergey <sergo920@yahoo.com>
2021-04-28 10:46:22 +0300

    Create IssueRefundRestResponse.php

---
+++ src/Fulfillment/Types/IssueRefundRestResponse.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace DTS\eBaySDK\Fulfillment\Types;
+
+use DTS\eBaySDK\HttpHeadersTrait;
+use DTS\eBaySDK\StatusCodeTrait;
+
+/**
+ * Created by PhpStorm.
+ * User: sergo
+ * Date: 4/27/21
+ * Time: 12:54 PM
+ */
+class IssueRefundRestResponse extends \DTS\eBaySDK\Types\BaseType
+{
+    use StatusCodeTrait;
+    use HttpHeadersTrait;
+
+    /**
+     * @var array Properties belonging to objects of this class.
+     */
+    private static $propertyTypes = [
+        'errors' => [
+            'type' => 'DTS\eBaySDK\Fulfillment\Types\ErrorDetailV3',
+            'repeatable' => true,
+            'attribute' => false,
+            'elementName' => 'errors'
+        ],
+        'warnings' => [
+            'type' => 'DTS\eBaySDK\Fulfillment\Types\ErrorDetailV3',
+            'repeatable' => true,
+            'attribute' => false,
+            'elementName' => 'warnings'
+        ]
+    ];
+
+    /**
+     * @param array $values Optional properties and values to assign to the object.
+     * @param int $statusCode Status code
+     * @param array $headers HTTP Response headers.
+     */
+    public function __construct(array $values = [], $statusCode = 200, array $headers = [])
+    {
+        list($parentValues, $childValues) = self::getParentValues(self::$propertyTypes, $values);
+
+        parent::__construct($parentValues);
+
+        if (!array_key_exists(__CLASS__, self::$properties)) {
+            self::$properties[__CLASS__] = array_merge(self::$properties[get_parent_class()], self::$propertyTypes);
+        }
+
+        $this->setValues(__CLASS__, $childValues);
+
+        $this->statusCode = (int)$statusCode;
+
+        $this->setHeaders($headers);
+    }
+}
