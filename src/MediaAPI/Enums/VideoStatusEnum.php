520e45540871da7ededffa09cfbae1439a2fd165
Sergey <sergo920@yahoo.com>
2021-07-08 07:59:52 +0300

    Create VideoStatusEnum.php

---
+++ src/MediaAPI/Enums/VideoStatusEnum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DTS\eBaySDK\MediaAPI\Enums;
+
+/**
+ * Class VideoStatusEnum
+ * @package DTS\eBaySDK\MediaAPI\Enums
+ * @link https://developer.ebay.com/api-docs/commerce/media/types/api:VideoStatusEnum
+ */
+class VideoStatusEnum
+{
+    const PENDING_UPLOAD = 'PENDING_UPLOAD';
+    const PROCESSING = 'PROCESSING';
+    const PROCESSING_FAILED = 'PROCESSING_FAILED';
+    const BLOCKED = 'BLOCKED';
+    const LIVE = 'LIVE';
+}
