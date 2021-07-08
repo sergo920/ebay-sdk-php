5fe60551b09961922a4897593368f2f25c4948eb
Sergey <sergo920@yahoo.com>
2021-07-08 07:59:15 +0300

    Create ClassificationEnum.php

---
+++ src/MediaAPI/Enums/ClassificationEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DTS\eBaySDK\MediaAPI\Enums;
+
+/**
+ * Class ClassificationEnum
+ * @package DTS\eBaySDK\MediaAPI\Enums
+ */
+class ClassificationEnum
+{
+    const ITEM = 'ITEM';
+}
