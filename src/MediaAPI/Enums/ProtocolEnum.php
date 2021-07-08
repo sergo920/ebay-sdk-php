32413a44eb03046e250d38f46a84dcbb7e38f309
Sergey <sergo920@yahoo.com>
2021-07-08 07:59:35 +0300

    Create ProtocolEnum.php

---
+++ src/MediaAPI/Enums/ProtocolEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DTS\eBaySDK\MediaAPI\Enums;
+
+/**
+ * Class ProtocolEnum
+ * @package DTS\eBaySDK\MediaAPI\Enums
+ * @link https://developer.ebay.com/api-docs/commerce/media/types/api:ProtocolEnum
+ */
+class ProtocolEnum
+{
+    const DASH = 'DASH';
+    const HLS = 'HLS';
+}
