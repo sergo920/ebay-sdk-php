2c2ca4b3fbe34304cd3e8b0b1d7141b4d491e3cc
Sergey <sergo920@yahoo.com>
2021-08-22 07:44:40 +0300

    Update TaxesType.php

--- src/Trading/Types/TaxesType.php
+++ src/Trading/Types/TaxesType.php
@@ -11,7 +11,7 @@
 namespace DTS\eBaySDK\Trading\Types;
 
 /**
- *
+ * @property string $eBayReference
  * @property \DTS\eBaySDK\Trading\Types\AmountType $TotalTaxAmount
  * @property \DTS\eBaySDK\Trading\Types\TaxDetailsType[] $TaxDetails
  */
@@ -21,6 +21,12 @@ class TaxesType extends \DTS\eBaySDK\Types\BaseType
      * @var array Properties belonging to objects of this class.
      */
     private static $propertyTypes = [
+        'eBayReference' => [
+            'type' => 'string',
+            'repeatable' => false,
+            'attribute' => false,
+            'elementName' => 'eBayReference'
+        ],
         'TotalTaxAmount' => [
             'type' => 'DTS\eBaySDK\Trading\Types\AmountType',
             'repeatable' => false,
